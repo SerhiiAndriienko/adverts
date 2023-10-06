4ce3ee0ef05dac42d7f6a18f2f842fe73fd36235
serhiiandriienko <116962959+SerhiiAndriienko@users.noreply.github.com>
2023-10-06 15:07:34 +0200

    Commit 1, just for a test

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,7 +4,7 @@ import { Routes, Route } from 'react-router-dom';
 import Hero from './component/Hero/Hero';
 import Home from './pages/home/Home';
 import Cars from './pages/cars/Cars';
-import Movies from './pages/movies/Movies';
+// import Movies from './pages/movies/Movies';
 import NotFound from './pages/notFound/NotFound';
 import Header from './component/header/Header';
 
